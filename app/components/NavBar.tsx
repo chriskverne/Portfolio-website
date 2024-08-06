2dd35457beeca1c1ba5a9cc1195db8e87456b5df
Christopher Lukas kverne <chris.kverne@gmail.com>
2024-08-06 19:00:27 +0200

    phone bg changes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TypeWriter from './TypeWriter';
 
 const NavBar = () => {
+  const [bgImage, setBgImage] = useState('/bg_pic6.png');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 640) {
+        setBgImage('/bg_pic1.jpg');
+      } else {
+        setBgImage('/bg_pic6.png');
+      }
+    };
+
+    // Set the initial background image based on the screen size
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Media query listener for devices that might not trigger resize
+    const mediaQuery = window.matchMedia('(max-width: 640px)');
+    const handleMediaQueryChange = (e) => {
+      if (e.matches) {
+        setBgImage('/bg_pic1.png');
+      } else {
+        setBgImage('/bg_pic6.png');
+      }
+    };
+
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    // Clean up the event listeners on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <div className='relative w-full '>
-      <img src='/bg_pic6.png' alt='Background' />
+    <div className='relative w-full'>
+      <img src={bgImage} alt='Background' />
       <TypeWriter />
     </div>
   );
