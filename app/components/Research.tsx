62a0486838efdb36bf43d285284ba00db180ff97
Christopher Lukas kverne <chris.kverne@gmail.com>
2024-08-10 18:33:52 +0200

    added quotes

--- app/components/Research.tsx
+++ app/components/Research.tsx
@@ -41,8 +41,8 @@ const Research = () => {
           <Title title={"Research"} />
         </div>
         <div className='w-4/5'>
-            <p>Im currently a junior at FIU working under the supervision of Professor Janki Bhimani in the <Link className='text-blue-600 underline' href={'https://damrl.cis.fiu.edu/'} target='_blank'>DaMRL Lab</Link>. My research interests include <b>Machine Learning</b>, <b>Computer Systems</b>, and <b>Practical AI</b>.</p>
-            <p className='mt-2'>My work dives into optimizing storage systems such as SSDs and RAM, optimizing ML phases such as ML training, and exploring use cases of AI in computer storage systems and other fields. After my bachelors, I will continue to pursue AI-related research in Grad school.</p>
+            <p>I&apos;m currently a junior at FIU working under the supervision of Professor Janki Bhimani in the <Link className='text-blue-600 underline' href={'https://damrl.cis.fiu.edu/'} target='_blank'>DaMRL Lab</Link>. My research interests include <b>Machine Learning</b>, <b>Computer Systems</b>, and <b>Practical AI</b>.</p>
+            <p className='mt-2'>My work dives into optimizing storage systems such as SSDs and RAM, optimizing ML phases such as ML training, and exploring use cases of AI in computer storage systems and other fields. After my bachelor&apos;s, I will continue to pursue AI-related research in Grad school.</p>
             <p className='font-semibold text-2xl mt-2'>Papers:</p>
             <Paper paper={paper1} />
             <Paper paper={paper2} />
