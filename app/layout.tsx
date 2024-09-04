17b18cd55a3ab07a7d63cef8b0c93f7ae462d77a
Christopher Kverne <chris.kverne@gmail.com>
2024-09-03 21:36:17 -0400

    tried to add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Christoper Kverne",
   description: "Christopher Kverne's Portfolio Website",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
