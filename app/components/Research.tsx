f221b1a6ac4ba0f24db251927164bb653da6657a
Christopher Kverne <chris.kverne@gmail.com>
2024-10-08 22:32:00 -0400

    fixed apostropeje bug

--- app/components/Research.tsx
+++ app/components/Research.tsx
@@ -12,7 +12,7 @@ const Research = () => {
       </div>
       <div className='w-4/5'>
         <p>I&apos;m currently a junior at FIU fortunate enough to be working under Professor <Link className='text-blue-600 underline' target='blank' href={'https://www.cis.fiu.edu/faculty-staff/janki-bhimani/'}>Janki Bhimani</Link> in the <Link className='text-blue-600 underline' href={'https://damrl.cis.fiu.edu/'} target='_blank'>DaMRL</Link> lab. My research interests include <b>Machine Learning</b>, <b>Computer Systems</b>, <b>Deep Learning</b>, and <b>Generative AI</b>.</p>
-        <p className='mt-2'>With the explosion of large language models, many companies use thousands of GPU's and months of training time to develop 1 single model. My goal is to reduce the computational power needed for this by developing more efficient deep learning architectures and optimizing processes such as parallel computing.</p>
+        <p className='mt-2'>With the explosion of large language models, many companies use thousands of GPU&apos;s and months of training time to develop 1 single model. My goal is to reduce the computational power needed for this by developing more efficient deep learning architectures and optimizing processes such as parallel computing.</p>
         <p className='font-semibold text-2xl mt-2'>Papers:</p>
         <Paper paper={paper1} />
         {/*<Paper paper={paper2} />*/}
