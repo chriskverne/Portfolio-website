ef3d4eeac240d4de15f3abfbd22876cde1d6b965
Christopher Kverne <chris.kverne@gmail.com>
2025-01-07 00:58:02 -0500

    changed text size and research description

--- app/components/Paper.tsx
+++ app/components/Paper.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Paper = ({paper}) => {
   return (
     <div className='mb-6 bg-[#e5e5e5] p-3 rounded-md'>
-      <p className='text-xl'>{paper.title}</p>
+      <p className='text-lg'>{paper.title}</p>
       {paper.isPublished && (
         <Link href={paper.link}>
           <p className='text-green-500 font-bold text-xl'>{paper.conference}</p>
@@ -12,11 +12,11 @@ const Paper = ({paper}) => {
       )}
 
       {paper.underReview && (
-        <p className='font-bold text-xl'>Under review at the {paper.conference} conference</p>
+        <p className='font-bold text-lg'>Under review at the {paper.conference} conference</p>
       )}
 
       {paper.workingOn && (
-        <p className='font-bold text-xl'>Currently working on it, Please read it when it comes out</p>
+        <p className='font-bold text-lg'>Currently working on it, Please read it when it comes out</p>
       )}
 
       <div>
--- app/components/Research.tsx
+++ app/components/Research.tsx
@@ -10,7 +10,7 @@ const Research = () => {
       <div className='w-4/5'>
         <Title title={"Research"} />
       </div>
-      <div className='w-4/5 text-xl'>
+      <div className='w-4/5 text-lg'>
         <p>
           I&apos;m currently a junior at FIU fortunate enough to be working under Professor <Link className='text-blue-600 underline' target='blank' href={'https://www.cis.fiu.edu/faculty-staff/janki-bhimani/'}>Janki Bhimani</Link> in 
           the <Link className='text-blue-600 underline' href={'https://damrl.cis.fiu.edu/'} target='_blank'>DaMRL</Link> lab.
@@ -19,7 +19,7 @@ const Research = () => {
         <p className='mt-2 underline'><i><b>What is intelligence?</b></i></p>
         <p className='mt-2'>
           This problem drives my current research interests.
-          While deep learning has demonstrated remarkable capabilities, it remains constrained by its reliance on large datasets and its limited ability to handle multimodal data effectively (e.g., integrating images, text, and audio). In contrast, humans seamlessly process and integrate diverse forms of information, such as sound, vision, and touch. As gradient-based learning continues to dominate the field of AI, I question whether it is always the optimal approach. My work seeks to explore the mathematical foundations of &quot;intelligence&quot; by advancing neural architectures and investigating alternative learning paradigms, such as biologically inspired networks. Ultimately, my goal is to deepen our understanding of machine learning foundations to replicate and enhance the concept of &quot;intelligence.&quot;
+          While deep learning has demonstrated remarkable capabilities, it remains constrained by its reliance on large datasets and its limited ability to handle multimodal data effectively (e.g., integrating images, text, and audio). As gradient-based learning continues to dominate the field of AI, I question whether it is always the optimal approach. My work seeks to explore the mathematical foundations of &quot;intelligence&quot; by advancing neural architectures and investigating alternative learning methods, such as biologically inspired networks or non-gradient learning. Ultimately, my goal is to deepen our understanding of machine learning foundations to replicate and enhance the concept of &quot;intelligence.&quot;
         </p>  
       </div>
       <div className='w-4/5'>
